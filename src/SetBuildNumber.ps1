034077503a8ea7c1f817c7a4a8018e9de7717b07
Rami Abughazaleh <rami.abughazaleh@gmail.com>
2020-04-08 17:21:32 -0700

    Automatically incrementing version number

---
+++ src/SetBuildNumber.ps1
@@ -0,0 +1,20 @@
+﻿##-----------------------------------------------------------------------
+## <copyright file="SetBuildNumber.ps1"></copyright>
+##-----------------------------------------------------------------------
+
+# Enable -Verbose option
+[CmdletBinding()]
+
+# Set a flag to force verbose as a default
+$VerbosePreference ='Continue' # equiv to -verbose
+
+$majorVersion = 0
+$minorVersion = 0
+$firstBuildYear = 2020
+
+$currentYear = Get-Date -Format yyyy
+$buildYear = [Convert]::ToInt32($currentYear)
+$currentMonthDay = Get-Date -Format MMdd
+$buildVersion = (($buildYear - $firstBuildYear) * 1200) + ([Convert]::ToInt32($currentMonthDay))
+
+Write-Host "##vso[build.updatebuildnumber]$majorVersion.$minorVersion.$buildVersion.$env:BUILD_COUNTER"
